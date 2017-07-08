d2820cf02888c06f4b57b9529c381e384a9551d8
Russ Butler <c1728p9@gmail.com>
2017-07-08 10:07:37 -0500

    Remove use of internal RTX types
    Make calls to cmsis-os to get thread state, stack size, and max stack
    usage rather than accessing internal RTX data directly. Wrap RTX5
    specific code in OS_BACKEND_RTX5.
    
    Also refactor the code to use mbed types rather than RTX types:
    os_timer_t -> mbed_rtos_storage_timer_t
    os_event_flags_t -> mbed_rtos_storage_event_flags_t
    osRtxMutex_t -> mbed_rtos_storage_thread_t

--- equeue_platform.h
+++ equeue_platform.h
@@ -48,7 +48,7 @@ extern "C" {
 #include "semphr.h"
 #elif defined(EQUEUE_PLATFORM_MBED)
 #include "cmsis_os2.h"
-#include "rtx_lib.h"
+#include "mbed_rtos_storage.h"
 #endif
 
 
@@ -116,7 +116,7 @@ typedef HANDLE equeue_sema_t;
 #elif defined(EQUEUE_PLATFORM_MBED) && defined(MBED_CONF_RTOS_PRESENT)
 typedef struct equeue_sema {
     osEventFlagsId_t id;
-    os_event_flags_t mem;
+    mbed_rtos_storage_event_flags_t mem;
 } equeue_sema_t;
 #elif defined(EQUEUE_PLATFORM_MBED)
 typedef volatile int equeue_sema_t;
